fde9ce41285df88311656b40d8db6becc0cbb97b
turkonjavla <turkonjavlada@gmail.com>
2018-03-06 17:24:50 +0100

    Add handlebars amd initial files

---
+++ app.js
@@ -0,0 +1,21 @@
+const exphbs  = require("express-handlebars"),
+      express = require("express"),
+      app     = express(),
+      port    = 5050 || process.env.PORT;
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.redirect("/ideas");
+});
+
+app.get("/ideas", (req, res) => {
+    res.render("index");
+});
+
+// Open connection
+app.listen(port, process.env.IP, () => {
+    console.log(`Server open at port: ${port}`)
+});
