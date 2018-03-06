9caec54760d12933d7d28a635c11201e49374b20
turkonjavla <turkonjavlada@gmail.com>
2018-03-06 19:39:53 +0100

    Add create idea logic

--- app.js
+++ app.js
@@ -5,27 +5,72 @@ const bodyParser = require("body-parser"),
       app        = express(),
       port       = 5050 || process.env.PORT;
 
+
+/* MODELS */
+ const Idea = require("./models/Idea"); 
+
+/* DB Connection */
 const db = process.env.DATABASEURL;
 mongoose.connect(db)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
+
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
+ 
+/* 
+###############
+    ROUTES
+###############
+ */
 
 app.get("/", (req, res) => {
     res.redirect("/ideas");
 });
 
+// INDEX
 app.get("/ideas", (req, res) => {
     res.render("index");
 });
 
+// NEW
 app.get("/ideas/new", (req, res) => {
     res.render("new");
 });
 
+ app.post("/ideas", (req, res) => {
+    let details  = req.body.details,
+        title    = req.body.title,
+        errors   = [],
+        ideaData = ({title: title, details: details});
+
+    if(!title) {
+        errors.push({text: "Please enter a title"});
+    }
+    if(!details) {
+        errors.push({text: "Pleaase enter some details."});
+    }
+
+    if(errors.length > 0) {
+        console.log(errors);
+        res.render("new", {errors: errors});
+    }
+    else {
+        new Idea(ideaData)
+            .save()
+            .then(idea => {
+                res.redirect("/ideas");
+            })
+            .catch(err => console.log(err));
+    }
+    
+}); 
+
+//ABOUT
 app.get("/about", (req, res) => {
     res.render("about");
 });
